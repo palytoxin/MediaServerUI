21624efe717daca1fc6f78b86b033912dd4d951f
kkkkk <1210399613@qq.com>
2020-04-28 14:51:23 +0800

    first

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
-const host = 'http://218.59.173.214:801/index/api';
+const host = 'http://localhost:801/index/api';
 const secret = '035c73f7-bb6b-4889-a715-d9eb2d1925cc';
-const baseMediaUrl='rtmp://218.59.173.214/';
+const baseMediaUrl='rtmp://localhost/';
 function genApiUrl(method){
 	return host+method+"?secret="+secret;
 }
